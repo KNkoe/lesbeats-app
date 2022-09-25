35f1a590fbdaf24d9b85f4dbaf8aaee8be782139
KNkoe <katleholnkoe@gmail.com>
2022-09-25 21:12:05 +0200

    animations

---
+++ lesbeats/lib/screens/chats/message.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyMessageScreen extends StatelessWidget {
+  const MyMessageScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
