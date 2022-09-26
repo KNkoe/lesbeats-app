0aca33a90f0df4a7e596371fd42134c968badcdf
KNkoe <katleholnkoe@gmail.com>
2022-09-26 15:56:35 +0200

    update UI

--- lesbeats/lib/screens/home/dashboard/lyrics.dart
+++ lesbeats/lib/screens/home/dashboard/lyrics.dart
@@ -34,6 +34,7 @@ class _MyLyrcisScreenState extends State<MyLyrcisScreen> {
   ];
 
   bool _viewAllArtists = false;
+  final PageController _pageController = PageController();
 
   bool _preview = false;
 
@@ -135,198 +136,177 @@ class _MyLyrcisScreenState extends State<MyLyrcisScreen> {
                 .toList(),
           ),
         ),
-        const SizedBox(
-          height: 20,
-        ),
         Expanded(
-          child: ListView.builder(
-              padding: EdgeInsets.zero,
-              shrinkWrap: true,
+          child: PageView.builder(
+              controller: _pageController,
               itemBuilder: (context, index) {
-                return Card(
-                  elevation: 2,
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Column(
-                      children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Row(
-                              children: [
-                                const CircleAvatar(
-                                  minRadius: 20,
-                                  backgroundImage:
-                                      AssetImage("assets/images/house.jpg"),
-                                ),
-                                const SizedBox(
-                                  width: 10,
-                                ),
-                                Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    const Text(
-                                      "Sunshine",
-                                      style: TextStyle(fontSize: 18),
-                                    ),
-                                    const SizedBox(
-                                      height: 4,
-                                    ),
-                                    Text(
-                                      artists[index],
-                                      style: const TextStyle(
-                                          color: Colors.black54),
-                                    ),
-                                  ],
-                                )
-                              ],
-                            ),
-                            Row(
-                              children: [
-                                Text(
-                                  "Posted ${DateTime.now().toString().split(" ")[0]}",
-                                  style: const TextStyle(color: Colors.grey),
-                                ),
-                                PopupMenuButton(
-                                    icon: const Icon(
-                                      Icons.more_vert,
-                                      color: Colors.black54,
-                                    ),
-                                    itemBuilder: ((context) => [
-                                          PopupMenuItem(
-                                              child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: const [
-                                              Icon(
-                                                Icons.chat,
-                                                color: Colors.black54,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text("Chat"),
-                                            ],
-                                          )),
-                                          PopupMenuItem(
-                                              child: Row(
-                                            mainAxisAlignment:
-                                                MainAxisAlignment.start,
-                                            children: const [
-                                              Icon(
-                                                Icons.check_circle,
-                                                color: Colors.black54,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text("Buy"),
-                                            ],
-                                          )),
-                                          const PopupMenuItem(
-                                              height: 2, child: Divider()),
-                                          PopupMenuItem(
-                                              child: Row(
-                                            children: const [
-                                              Icon(
-                                                Icons.share,
-                                                color: Colors.black54,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text("Share"),
-                                            ],
-                                          )),
-                                          PopupMenuItem(
-                                              child: Row(
-                                            children: const [
-                                              Icon(
-                                                Icons.report,
-                                                color: Colors.black54,
-                                              ),
-                                              SizedBox(
-                                                width: 10,
-                                              ),
-                                              Text("Report"),
-                                            ],
-                                          )),
-                                        ])),
-                              ],
-                            )
-                          ],
-                        ),
-                        Container(
-                          height: _preview ? 400 : 200,
-                          margin: const EdgeInsets.all(10),
-                          padding: const EdgeInsets.fromLTRB(20, 20, 10, 10),
-                          decoration: BoxDecoration(
-                              border: Border.all(color: Colors.black12),
-                              borderRadius: BorderRadius.circular(10)),
-                          child: Stack(
+                return Container(
+                  padding: const EdgeInsets.all(30),
+                  child: Card(
+                    elevation: 15,
+                    child: Padding(
+                      padding: const EdgeInsets.all(14),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
-                              const Text(
-                                "Verse 1\nEverything is soo different now\nI never thought I'd be the one to say that I'm in love\nI never caught myself obessing over one girl\nAnd I promise its the best feeling I ever had\nI spend most night just thinking about it\nGirl got me crazy I only hear this when I read about it\nIt's soo much like in pradise,\nI mean everytime I wake up next to her I can't believe my eyes",
-                                overflow: TextOverflow.fade,
-                                style: TextStyle(
-                                    color: Colors.black54, fontSize: 18),
+                              Row(
+                                children: [
+                                  const CircleAvatar(
+                                    minRadius: 20,
+                                    backgroundImage:
+                                        AssetImage("assets/images/house.jpg"),
+                                  ),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  Column(
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.start,
+                                    children: [
+                                      const Text(
+                                        "Sunshine",
+                                        style: TextStyle(fontSize: 18),
+                                      ),
+                                      const SizedBox(
+                                        height: 4,
+                                      ),
+                                      Text(
+                                        artists[index],
+                                        style: const TextStyle(
+                                            color: Colors.black54),
+                                      ),
+                                    ],
+                                  )
+                                ],
                               ),
-                              Positioned(
-                                right: 0,
-                                bottom: 0,
-                                child: ElevatedButton(
-                                    style: ElevatedButton.styleFrom(
-                                        elevation: 1,
-                                        backgroundColor: Colors.black12,
-                                        shape: const CircleBorder()),
-                                    onPressed: () {
-                                      setState(() {
-                                        _preview = !_preview;
-                                      });
-                                    },
-                                    child: Icon(_preview
-                                        ? Icons.arrow_drop_up
-                                        : Icons.arrow_drop_down)),
+                              Row(
+                                children: [
+                                  Text(
+                                    "Posted ${DateTime.now().toString().split(" ")[0]}",
+                                    style: const TextStyle(color: Colors.grey),
+                                  ),
+                                  PopupMenuButton(
+                                      icon: const Icon(
+                                        Icons.more_vert,
+                                        color: Colors.black54,
+                                      ),
+                                      itemBuilder: ((context) => [
+                                            PopupMenuItem(
+                                                child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              children: const [
+                                                Icon(
+                                                  Icons.chat,
+                                                  color: Colors.black54,
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text("Chat"),
+                                              ],
+                                            )),
+                                            PopupMenuItem(
+                                                child: Row(
+                                              mainAxisAlignment:
+                                                  MainAxisAlignment.start,
+                                              children: const [
+                                                Icon(
+                                                  Icons.check_circle,
+                                                  color: Colors.black54,
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text("Buy"),
+                                              ],
+                                            )),
+                                            const PopupMenuItem(
+                                                height: 2, child: Divider()),
+                                            PopupMenuItem(
+                                                child: Row(
+                                              children: const [
+                                                Icon(
+                                                  Icons.share,
+                                                  color: Colors.black54,
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text("Share"),
+                                              ],
+                                            )),
+                                            PopupMenuItem(
+                                                child: Row(
+                                              children: const [
+                                                Icon(
+                                                  Icons.report,
+                                                  color: Colors.black54,
+                                                ),
+                                                SizedBox(
+                                                  width: 10,
+                                                ),
+                                                Text("Report"),
+                                              ],
+                                            )),
+                                          ])),
+                                ],
                               )
                             ],
                           ),
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                          children: [
-                            Padding(
-                              padding: const EdgeInsets.only(left: 20),
-                              child: Row(
-                                children: const [
-                                  Icon(
-                                    Icons.thumb_up,
-                                    color: Colors.blue,
-                                  ),
-                                  SizedBox(
-                                    width: 4,
-                                  ),
-                                  Text(
-                                    "54",
-                                    style: TextStyle(color: Colors.black54),
-                                  ),
-                                ],
-                              ),
+                          Container(
+                            height: 200,
+                            margin: const EdgeInsets.all(10),
+                            padding: const EdgeInsets.fromLTRB(20, 20, 10, 10),
+                            child: const Text(
+                              "Verse 1\nEverything is soo different now\nI never thought I'd be the one to say that I'm in love\nI never caught myself obessing over one girl\nAnd I promise its the best feeling I ever had\nI spend most night just thinking about it\nGirl got me crazy I only hear this when I read about it\nIt's soo much like in pradise,\nI mean everytime I wake up next to her I can't believe my eyes",
+                              overflow: TextOverflow.fade,
+                              style: TextStyle(
+                                  color: Colors.black54, fontSize: 18),
                             ),
-                            Padding(
-                              padding: const EdgeInsets.only(right: 20),
-                              child: ElevatedButton.icon(
-                                  icon: const Text(
-                                    " R",
-                                    style: TextStyle(
-                                        fontSize: 20,
-                                        fontWeight: FontWeight.bold),
-                                  ),
+                          ),
+                          const Spacer(),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              (index != 0)
+                                  ? IconButton(
+                                      onPressed: () {
+                                        _pageController.previousPage(
+                                            duration:
+                                                const Duration(seconds: 1),
+                                            curve: Curves.ease);
+                                      },
+                                      icon: const Icon(
+                                        Icons.arrow_back_ios_rounded,
+                                        color: Colors.black26,
+                                      ))
+                                  : const SizedBox(
+                                      width: 20,
+                                    ),
+                              OutlinedButton(
+                                  style: OutlinedButton.styleFrom(
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius:
+                                              BorderRadius.circular(20))),
                                   onPressed: () {},
-                                  label: const Text("100")),
-                            ),
-                          ],
-                        )
-                      ],
+                                  child: const Text("R100")),
+                              IconButton(
+                                  onPressed: () {
+                                    _pageController.nextPage(
+                                        duration: const Duration(seconds: 1),
+                                        curve: Curves.ease);
+                                  },
+                                  icon: const Icon(
+                                    Icons.arrow_forward_ios_rounded,
+                                    color: Colors.black26,
+                                  ))
+                            ],
+                          )
+                        ],
+                      ),
                     ),
                   ),
                 );
