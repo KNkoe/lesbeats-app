55ae9677c36c677927d076ab139d18d1f2e93319
KNkoe <katleholnkoe@gmail.com>
2022-12-10 20:03:36 +0200

    update: recode deleted files and code

---
+++ lesbeats/lib/widgets/load.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+
+import 'responsive.dart';
+
+class LoadTrack extends StatelessWidget {
+  const LoadTrack({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 20),
+        child: Animate(
+          effects: const [ShimmerEffect(duration: Duration(seconds: 1))],
+          onComplete: ((controller) {
+            controller.repeat();
+          }),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                height: 30,
+                width: 40,
+                decoration: const BoxDecoration(
+                    color: Colors.black12, shape: BoxShape.circle),
+              ),
+              const SizedBox(
+                width: 20,
+              ),
+              Container(
+                height: 30,
+                width: screenSize(context).width * 0.6,
+                decoration: BoxDecoration(
+                    color: Colors.black12,
+                    borderRadius: BorderRadius.circular(20)),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
